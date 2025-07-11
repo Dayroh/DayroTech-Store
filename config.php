01b6ac4aaf833e8d2b9201aba433bc6f0aca9f72
Theetilen19 <ochiengtilen5@gmail.com>
2025-07-11 15:55:48 +0300

    Config file modified

--- config.php
+++ config.php
@@ -1,14 +1,13 @@
 <?php
-$host = "localhost";
-//$host = "mysql.railway.internal";        
-//$port = "3306";                
+
+$host = "mysql.railway.internal";        
+$port = "3306";                
 $user = "root";
-//$pass = "rrdsmfeUcAgQUYjXYGGtNsUkXEnsKzvQ"; 
-$pass = ""; // Updated password
+$pass = "rrdsmfeUcAgQUYjXYGGtNsUkXEnsKzvQ"; 
 $dbname = "railway";
 
-//$conn = new mysqli("$host:$port", $user, $pass, $dbname);
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli("$host:$port", $user, $pass, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
