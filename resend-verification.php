cb8eb4a38c49fca7ebfc49f44d1d29a8b3b8ea87
Dayroh <124040732+Dayroh@users.noreply.github.com>
2025-07-25 15:55:54 +0300

    Update resend-verification.php

--- resend-verification.php
+++ resend-verification.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include('config.php'); // Your DB connection
-include('mail.php'); // PHPMailer setup
+include 'includes/mail.php'; // PHPMailer setup
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
